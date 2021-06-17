de9e64704cb37baeaff7782fc3578bd965f5cc3a
mefioo <maciek1271@gmail.com>
2021-06-17 15:15:03 +0200

    Adding js to the menu__link and scroll

--- app.js
+++ app.js
@@ -13,6 +13,31 @@ const menuShow = () => {
     });
 }
 
+
+
+
+
+
+const menuLinkClick = () => {
+$('.menu__link').on('click', function () {
+
+$('body, html').animate({
+    scrollTop: $('[data-section=' + $(this).attr('id') + ']').offset().top
+}, 1000);
+
+$('.menu-container').toggleClass('js-menu-container--show');
+
+$('.burger__line--line1').toggleClass('js-burger__line1--active');
+$('.burger__line--line2').toggleClass('js-burger__line2--active');
+$('.burger__line--line3').toggleClass('js-burger__line3--active');
+
+});
+}
+
+
+
+
+
 const scrollToBottom = () => {
     $('.to-bottom').on('click', function () {
         $('body, html').animate({
@@ -30,56 +55,27 @@ const scrollToTop = () => {
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
 const scrollPage = () => {
     $(window).on('scroll', function () {
         const scrollValue = $(window).scrollTop();
-        const distanceAbout = $('.section--about').offset().top - 250;
-        const distancePortfolio = $('.section--portfolio').offset().top - 250;
-        const distanceOffer = $('.section--offer').offset().top - 250;
-        const distanceContact = $('.section--contact').offset().top - 250;
+        const distanceAbout = $('.section--about').offset().top - 1;
 
-        // navLinkActive
+        // toBottomHide / toTopShow
+        if (scrollValue > distanceAbout) {
+            $('.to-bottom').addClass('js-to-bottom--hide');
+            $('.to-top').addClass('js-to-top--show');
+        }
         if (scrollValue < distanceAbout) {
-            $('.menu__underscore').not('.menu__underscore--1').removeClass('menu__underscore--active');
-            $('.menu__underscore--1').addClass('menu__underscore--active');
-        } else if (scrollValue < distancePortfolio) {
-            $('.menu__underscore').not('.menu__underscore--2').removeClass('menu__underscore--active');
-            $('.menu__underscore--2').addClass('menu__underscore--active');
-        } else if (scrollValue < distanceOffer) {
-            $('.menu__underscore').not('.menu__underscore--3').removeClass('menu__underscore--active');
-            $('.menu__underscore--3').addClass('menu__underscore--active');
-        } else if (scrollValue < distanceContact) {
-            $('.menu__underscore').not('.menu__underscore--4').removeClass('menu__underscore--active');
-            $('.menu__underscore--4').addClass('menu__underscore--active');
-        } else {
-            $('.menu__underscore').not('.menu__underscore--5').removeClass('menu__underscore--active');
-            $('.menu__underscore--5').addClass('menu__underscore--active');
+            $('.to-bottom').removeClass('js-to-bottom--hide');
+            $('.to-top').removeClass('js-to-top--show');
         }
 
-        // toTopShow
-        if (scrollValue > distanceAbout / 2) {
-            $('.to-top').addClass('to-top--show');
-        }
-        if (scrollValue < distanceAbout / 2) {
-            $('.to-top').removeClass('to-top--show');
-        }
     })
 }
 
 const app = () => {
     menuShow();
+    menuLinkClick();
     scrollToBottom();
     scrollToTop();
     scrollPage();
